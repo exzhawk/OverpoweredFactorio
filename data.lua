f2f920963e2b4645b6bbbf74a0e07ed5ae410ee2
Epix <git@exz.me>
2016-07-04 20:56:12 +0800

    initial demo

---
+++ data.lua
@@ -0,0 +1,12 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Epix
+-- Date: 2016/7/4
+-- Time: 18:19
+-- To change this template use File | Settings | File Templates.
+--
+
+require("prototypes.item")
+--require("prototypes.entity")
+--require("prototypes.recipe")
+--require("prototypes.technology")
